ba756fc4370bfd3bef8625f2274632cfeb6fd999
Adeeb Rahman <adeeb.rahman@mail.utoronto.ca>
2021-01-28 00:28:36 -0500

    added ability to login/logout using amazon cognito

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import '../css/containers/Dashboard.css'
 import CarCard from '../components/CarCard'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container,
+    Row, 
+    Col,
+    Button } from 'react-bootstrap';
+
 
 class Dashboard extends React.Component {
     render(){
         return(
             <div className= "parent_container_dashboard">
-                <h4 className="user_welcome">Welcome, John Doe</h4>
+                <div className="user_welcome">
+                    <h4 className="user_welcome_text">Welcome, John Doe</h4>
+                    
+                    {/* Note: logout_uri needs to be EXACT SAME as the signout url in cognito console */}
+                    <Button 
+                    variant="outline-success" 
+                    href="https://agltest.auth.us-east-1.amazoncognito.com/logout?client_id=6udbap9k9c743qirm1g4flp595&logout_uri=https://master.d8mrzxl42byhr.amplifyapp.com/">
+                        Logout
+                    </Button>
+                
+                </div>
+                
+                
                 <Container className = "car_list mt-5">
                     <Row>
                         <Col sm>
@@ -20,6 +36,9 @@ class Dashboard extends React.Component {
                             <CarCard carName={"Rolls-Royce Phantom"} carStatus={"Unavailable"}/>
                         </Col>
                     </Row>
+                    
+    
+
                 </Container>                
             </div>
         )
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,11 +10,10 @@ class Home extends React.Component {
                 <h1 className="title">AGL WebApp</h1>
                 
                 <div className="signup_login">
-                    <Button variant="outline-success" >
-                        SignUp
-                    </Button>
-                    <Button variant="outline-success" href="/dashboard">
-                        Login
+                    <Button 
+                    variant="outline-success" 
+                    href="https://agltest.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=6udbap9k9c743qirm1g4flp595&redirect_uri=https://master.d8mrzxl42byhr.amplifyapp.com/dashboard">
+                        SignUp / Login
                     </Button>
                 </div>
                
